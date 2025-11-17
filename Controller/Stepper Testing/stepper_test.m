9165354b725445c2751e3f949566852e96bc8e2e
arclnx <73797060+arclnx@users.noreply.github.com>
2025-11-17 13:39:09 -0500

    matlab

--- Controller/Stepper Testing/stepper_test.m
+++ Controller/Stepper Testing/stepper_test.m
@@ -5,13 +5,21 @@
 %a = arduino('COM3', 'Mega2560');   % change COM port if needed
 
 % Pin assignments
-stepPin = 'D53';
-dirPin  = 'D52';
+stepPin1 = 'D53';
+dirPin1  = 'D52';
+stepPin2 = 'D50';
+dirPin2  = 'D51';
+stepPin3 = 'D49';
+dirPin3  = 'D48';
 enPin   = 'D6';   % optional
 
 % Configure pins
-configurePin(a, stepPin, 'DigitalOutput');
-configurePin(a, dirPin,  'DigitalOutput');
+configurePin(a, stepPin1, 'DigitalOutput');
+configurePin(a, dirPin1,  'DigitalOutput');
+configurePin(a, stepPin2, 'DigitalOutput');
+configurePin(a, dirPin2,  'DigitalOutput');
+configurePin(a, stepPin3, 'DigitalOutput');
+configurePin(a, dirPin3,  'DigitalOutput');
 configurePin(a, enPin,   'DigitalOutput');
 
 % Enable driver (LOW = enabled on DRV8825)
@@ -19,18 +27,30 @@ writeDigitalPin(a, enPin, 0);
 
 % Motion parameters
 steps = 1000;        % number of steps to move
-stepDelay = 0.01;   % delay between steps (1 ms ≈ 1000 steps/sec)
+stepDelay = 0.001;   % delay between steps (1 ms ≈ 1000 steps/sec)
 direction = 1;       % 1 = CW, 0 = CCW
 
 % Set direction
-writeDigitalPin(a, dirPin, direction);
+writeDigitalPin(a, dirPin1, direction);
+writeDigitalPin(a, dirPin2, direction);
+writeDigitalPin(a, dirPin3, direction);
+
 
 % Generate step pulses
 for i = 1:steps
-    writeDigitalPin(a, stepPin, 1);
+    fprintf("step %d\n", i)
+    writeDigitalPin(a, stepPin1, 1);
+    writeDigitalPin(a, stepPin1, 0);
     pause(stepDelay);
-    writeDigitalPin(a, stepPin, 0);
+
+    writeDigitalPin(a, stepPin2, 1);
+    writeDigitalPin(a, stepPin2, 0);
     pause(stepDelay);
+
+    writeDigitalPin(a, stepPin3, 1);
+    writeDigitalPin(a, stepPin3, 0);
+    pause(stepDelay);
+
 end
 
 % Optional: Disable motor
