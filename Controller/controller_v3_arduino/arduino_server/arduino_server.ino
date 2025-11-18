fef671fb1b998544607b5867a9e56880e2e83358
arclnx <73797060+arclnx@users.noreply.github.com>
2025-11-17 19:42:48 -0500

    working on controller, serial interface in serial interface.txt

---
+++ Controller/controller_v3_arduino/arduino_server/arduino_server.ino
@@ -0,0 +1,96 @@
+// Libraries
+#include <Wire.h>
+
+// Pressure Transducers
+#define MUX_ADDR 0x70
+#define PT_ADDR 0x6D
+
+// Strain Gauges
+#define SG1_SCK
+#define SG1_DATA
+#define SG2_SCK
+#define SG2_DATA
+#define SG3_SCK
+#define SG3_DATA
+
+// Steper Motors
+#define M1_IN1
+#define M1_IN2
+#define M1_IN3
+#define M1_IN4
+#define M2_IN1
+#define M2_IN2
+#define M2_IN3
+#define M2_IN4
+#define M3_IN1
+#define M3_IN2
+#define M3_IN3
+#define M3_IN4
+
+// Serial
+#define BAUD 115200
+void setup() {
+  // Setup serial
+  Serial.begin(BAUD);
+  while (!Serial);
+
+  // Setup I2C
+  Wire.begin();
+}
+
+void loop() {
+  int32_t raw_pressure;
+
+  if (1) {
+    mux_select(0);
+    read_pt(&raw_pressure);
+    Serial.print("PT1: " + String(raw_pressure,10) + "\n");
+
+    mux_select(1);
+    read_pt(&raw_pressure);
+    Serial.print("PT2: " + String(raw_pressure,10) + "\n");
+
+    mux_select(2);
+    read_pt(&raw_pressure);
+    Serial.print("PT3: " + String(raw_pressure,10) + "\n\n");
+  }
+  delay(150);
+}
+
+uint8_t read_pt(int32_t *raw_pressure) {
+  // variables
+  uint8_t pressure_H;
+  uint8_t pressure_M;
+  uint8_t pressure_L;
+  int32_t pressure_adc;
+
+  // tell sensor to read pressure and temp
+  Wire.beginTransmission(PT_ADDR);
+  Wire.write(0x30);
+  Wire.write(0x0A);
+  Wire.endTransmission();
+
+  // read values from pressure sensor
+  Wire.beginTransmission(PT_ADDR);
+  Wire.write(0x06);
+  Wire.endTransmission();
+
+
+  Wire.requestFrom(PT_ADDR, 3);
+  pressure_H = Wire.read();
+  pressure_M = Wire.read();
+  pressure_L = Wire.read();
+
+  pressure_adc = ((uint32_t)pressure_H << 8) + (uint32_t) pressure_M;
+  pressure_adc = (pressure_adc << 8) + (uint32_t) pressure_L;
+  *raw_pressure = ((pressure_adc << 8) >> 8);
+}
+
+void mux_select(uint8_t i) {
+  // select device i from multiplexer
+  if (i > 7) return;
+ 
+  Wire.beginTransmission(MUX_ADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+}
