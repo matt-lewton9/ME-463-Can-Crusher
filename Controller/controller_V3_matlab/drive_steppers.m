a8362b8b11685431d3d8fab62e44072b2d1ef88c
arclnx <73797060+arclnx@users.noreply.github.com>
2025-11-19 12:13:14 -0500

    update drive steppers.m to include direction and clip values sent

--- Controller/controller_V3_matlab/drive_steppers.m
+++ Controller/controller_V3_matlab/drive_steppers.m
@@ -1,10 +1,17 @@
-function drive_steppers(s, Steps1, Steps2, Steps3)
+function drive_steppers(s, m1_dir, m1_steps, m2_dir, m2_steps, m3_dir, m3_steps)
+% commands stepper motors to move 
 
 write(s,"2","uint8"); %poke arduino
 read(s,1,"uint8");
 
-write(s, Steps1,"uint8")
-write(s, Steps2,"uint8")
-write(s, Steps3,"uint8")
+% write stepper commands
+% clip direction to [0 1]
+% clipper steps to [0 255]
+write(s, clip(m1_dir, 0, 1), "uint8")
+write(s, clip(m1_steps, 0, 255), "uint8")
+write(s, clip(m2_dir, 0, 1), "uint8")
+write(s, clip(m2_steps, 0, 255), "uint8")
+write(s, clip(m3_dir, 0, 1), "uint8")
+write(s, clip(m3_steps, 0, 255), "uint8")
 
 end
