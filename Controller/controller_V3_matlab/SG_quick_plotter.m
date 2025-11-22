1fa843a2927b9f59b74225e29d24c1aa9f445d05
arclnx <73797060+arclnx@users.noreply.github.com>
2025-11-21 21:15:36 -0500

    pushypushhy pweeze

---
+++ Controller/controller_V3_matlab/SG_quick_plotter.m
@@ -0,0 +1,9 @@
+
+clc;
+close all;
+plot(time,SGs(1,:))
+plot(times,SGs(1,:))
+hold on;
+plot(times,SGs(2,:))
+plot(times,SGs(3,:))
+legend("SG1","SG2","SG3")
---
+++ Controller/controller_V3_matlab/test_campaign.m
@@ -0,0 +1,6 @@
+try
+    fsm_main
+catch ERR
+    disp(ERR)
+end
+write_outputs
