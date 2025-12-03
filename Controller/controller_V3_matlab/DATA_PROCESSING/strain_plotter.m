e7d7fa16ccd7d2b60b06ae04acdebc679280e6cd
Matthew Lewton <matt.lewton9@gmail.com>
2025-12-03 00:45:14 -0500

    fea model

--- Controller/controller_V3_matlab/DATA_PROCESSING/strain_plotter.m
+++ Controller/controller_V3_matlab/DATA_PROCESSING/strain_plotter.m
@@ -67,38 +67,38 @@ ylabel("Strain [\muin / in] ")
 
 
 
-% figure(randi(1000))
-% 
-% 
-% plot(data_times,PTs(1,:).*CYLINDER_AREA);
-% hold on
-% plot(data_times,PTs(2,:).*CYLINDER_AREA);
-% hold on
-% plot(data_times,PTs(3,:).*CYLINDER_AREA);
-% legend("Actuator 1","Actuator 2","Actuator 3", 'Location', 'southeast')
-% title(sprintf("%s Forces", test_name_in));
-% xlabel("Time [s]")
-% ylabel("Force [lbf] ")
-
-
 figure(randi(1000))
 
 
-plot(data_times ,(SGs(2,:)'-e1)./e1.*100, 'Color',"#33C5FF"); % SG 1 and 2 are crossed somewhere,check wiring
+plot(data_times,PTs(1,:).*CYLINDER_AREA);
 hold on
-plot(data_times,(SGs(1,:)'-e2)./e2.*100, 'Color',"#FFA333");
+plot(data_times,PTs(2,:).*CYLINDER_AREA);
 hold on
-plot(data_times,(SGs(3,:)'-e3)./e3.*100, 'Color',"#C233FF");
-hold on
-plot([0, max(data_times)], [10 10], 'r--')
-hold on
-plot([0, max(data_times)], [-10 -10], 'r--')
-plot([0, max(data_times)], [0 0], 'g--')
-legend("SG 1","SG 2","SG 3", 'Location', 'northwest')
-title(sprintf("%s Percent Error", test_name_in));
+plot(data_times,PTs(3,:).*CYLINDER_AREA);
+legend("Actuator 1","Actuator 2","Actuator 3", 'Location', 'southeast')
+title(sprintf("%s Forces", test_name_in));
 xlabel("Time [s]")
-ylabel("Percent Error [%] ")
-ylim([-20 20])
+ylabel("Force [lbf] ")
+
+
+% figure(randi(1000))
+% 
+% 
+% plot(data_times ,(SGs(2,:)'-e1)./e1.*100, 'Color',"#33C5FF"); % SG 1 and 2 are crossed somewhere,check wiring
+% hold on
+% plot(data_times,(SGs(1,:)'-e2)./e2.*100, 'Color',"#FFA333");
+% hold on
+% plot(data_times,(SGs(3,:)'-e3)./e3.*100, 'Color',"#C233FF");
+% hold on
+% plot([0, max(data_times)], [10 10], 'r--')
+% hold on
+% plot([0, max(data_times)], [-10 -10], 'r--')
+% plot([0, max(data_times)], [0 0], 'g--')
+% legend("SG 1","SG 2","SG 3", 'Location', 'northwest')
+% title(sprintf("%s Percent Error", test_name_in));
+% xlabel("Time [s]")
+% ylabel("Percent Error [%] ")
+% ylim([-20 20])
 
 clear;
 end
