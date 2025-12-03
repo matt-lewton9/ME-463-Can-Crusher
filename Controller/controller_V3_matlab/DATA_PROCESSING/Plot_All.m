f80c81ddfacd4fd7ebb9f5ea4638c1a206815d83
Matthew Lewton <matt.lewton9@gmail.com>
2025-12-03 12:07:02 -0500

    add SG offsets

--- Controller/controller_V3_matlab/DATA_PROCESSING/Plot_All.m
+++ Controller/controller_V3_matlab/DATA_PROCESSING/Plot_All.m
@@ -7,15 +7,15 @@ clear
 % strain_plotter("Max Compression","max_com_03_30-Nov-2025_18-33-58")
 % 
 % 
-strain_plotter("Max Bending 180 Deg","max_bend_180_1_30-Nov-2025_19-47-52")
-strain_plotter("Max Bending 0 Deg","max_bend_0deg_2_30-Nov-2025_19-21-48")
+strain_plotter("Max Bending 180 Deg","max_bend_180_1_30-Nov-2025_19-47-52", [-12 42 57])
+strain_plotter("Max Bending 0 Deg","max_bend_0deg_2_30-Nov-2025_19-21-48", [-12 42 57])
 
-strain_plotter("Max Bending 120 Deg","Max_Bend_120deg_1_30-Nov-2025_19-27-42")
+strain_plotter("Max Bending 120 Deg","Max_Bend_120deg_1_30-Nov-2025_19-27-42", [-12 43 57])
 
-strain_plotter("Max Bending 300 Deg","max_comp_300_1_01-Dec-2025_16-55-37")
-strain_plotter("Max Bending 240 Deg","max_bend_240_1_01-Dec-2025_17-33-58")
+strain_plotter("Max Bending 300 Deg","max_comp_300_1_01-Dec-2025_16-55-37", [-15 45 55])
+strain_plotter("Max Bending 240 Deg","max_bend_240_1_01-Dec-2025_17-33-58", [-15 45 55])
 
-strain_plotter("Max Compression","max_comp_5_01-Dec-2025_17-04-24")
+strain_plotter("Max Compression","max_comp_4_30-Nov-2025_18-46-49", [-8 44 49])
 
 
 
--- Controller/controller_V3_matlab/DATA_PROCESSING/strain_plotter.m
+++ Controller/controller_V3_matlab/DATA_PROCESSING/strain_plotter.m
@@ -1,4 +1,4 @@
-function [] = strain_plotter(test_name_in, path_name)
+function [] = strain_plotter(test_name_in, path_name, de)
 
 %% Data
 
@@ -18,9 +18,9 @@ SGs(2,:) = filloutliers(SGs(2,:),'linear',"movmedian",10);
 SGs(3,:) = filloutliers(SGs(3,:),'linear',"movmedian",10);
 
 
-SGs(1,:) = SGs(1,:) + (46) ;
-SGs(2,:) = SGs(2,:) + (-17);
-SGs(3,:) = SGs(3,:) + (55);
+SGs(1,:) = SGs(1,:) + de(2) ;
+SGs(2,:) = SGs(2,:) + de(1);
+SGs(3,:) = SGs(3,:) + de(3);
 
 
 [e1, e2, e3] = strain_predict(PTs(1,:)'.*CYLINDER_AREA, PTs(2,:)'.*CYLINDER_AREA, PTs(3,:)'.*CYLINDER_AREA);
@@ -67,38 +67,37 @@ ylabel("Strain [\muin / in] ")
 
 
 
+% figure(randi(1000))
+% 
+% plot(data_times,PTs(1,:).*CYLINDER_AREA);
+% hold on
+% plot(data_times,PTs(2,:).*CYLINDER_AREA);
+% hold on
+% plot(data_times,PTs(3,:).*CYLINDER_AREA);
+% legend("Actuator 1","Actuator 2","Actuator 3", 'Location', 'southeast')
+% title(sprintf("%s Forces", test_name_in));
+% xlabel("Time [s]")
+% ylabel("Force [lbf] ")
+
+
 figure(randi(1000))
 
 
-plot(data_times,PTs(1,:).*CYLINDER_AREA);
+plot(data_times ,(SGs(2,:)'-e1)./e1.*100, 'Color',"#33C5FF"); % SG 1 and 2 are crossed somewhere,check wiring
 hold on
-plot(data_times,PTs(2,:).*CYLINDER_AREA);
+plot(data_times,(SGs(1,:)'-e2)./e2.*100, 'Color',"#FFA333");
 hold on
-plot(data_times,PTs(3,:).*CYLINDER_AREA);
-legend("Actuator 1","Actuator 2","Actuator 3", 'Location', 'southeast')
-title(sprintf("%s Forces", test_name_in));
+plot(data_times,(SGs(3,:)'-e3)./e3.*100, 'Color',"#C233FF");
+hold on
+plot([0, max(data_times)], [10 10], 'r--')
+hold on
+plot([0, max(data_times)], [-10 -10], 'r--')
+plot([0, max(data_times)], [0 0], 'g--')
+legend("SG 1","SG 2","SG 3", 'Location', 'northwest')
+title(sprintf("%s Percent Error", test_name_in));
 xlabel("Time [s]")
-ylabel("Force [lbf] ")
-
-
-% figure(randi(1000))
-% 
-% 
-% plot(data_times ,(SGs(2,:)'-e1)./e1.*100, 'Color',"#33C5FF"); % SG 1 and 2 are crossed somewhere,check wiring
-% hold on
-% plot(data_times,(SGs(1,:)'-e2)./e2.*100, 'Color',"#FFA333");
-% hold on
-% plot(data_times,(SGs(3,:)'-e3)./e3.*100, 'Color',"#C233FF");
-% hold on
-% plot([0, max(data_times)], [10 10], 'r--')
-% hold on
-% plot([0, max(data_times)], [-10 -10], 'r--')
-% plot([0, max(data_times)], [0 0], 'g--')
-% legend("SG 1","SG 2","SG 3", 'Location', 'northwest')
-% title(sprintf("%s Percent Error", test_name_in));
-% xlabel("Time [s]")
-% ylabel("Percent Error [%] ")
-% ylim([-20 20])
+ylabel("Percent Error [%] ")
+ylim([-20 20])
 
 clear;
 end
