3d495e2602382a6630c524a85f3d1eac58640cd5
Matthew Lewton <matt.lewton9@gmail.com>
2025-12-12 01:17:54 -0500

    Updated drawings

--- Controller/controller_V3_matlab/DATA_PROCESSING/Plot_All.m
+++ Controller/controller_V3_matlab/DATA_PROCESSING/Plot_All.m
@@ -7,15 +7,15 @@ clear
 % strain_plotter("Max Compression","max_com_03_30-Nov-2025_18-33-58")
 % 
 % 
-strain_plotter("Max Bending 180 Deg","max_bend_180_1_30-Nov-2025_19-47-52", [-12 42 57])
+
+% strain_plotter("Max Compression","max_comp_03-Dec-2025_17-15-08", [-8 56 29])
 strain_plotter("Max Bending 0 Deg","max_bend_0deg_2_30-Nov-2025_19-21-48", [-12 42 57])
+% strain_plotter("Max Bending 60 Deg","max_bend_60_deg_03-Dec-2025_16-51-30", [-8 56 29])
+% strain_plotter("Max Bending 120 Deg","Max_Bend_120deg_1_30-Nov-2025_19-27-42", [-12 43 57])
+% strain_plotter("Max Bending 180 Deg","max_bend_180_1_30-Nov-2025_19-47-52", [-12 42 57])
+% strain_plotter("Max Bending 240 Deg","max_bend_240_deg_03-Dec-2025_16-58-55", [-3 50 35]) 
+strain_plotter("Max Bending 300 Deg","max_bend_300_deg_03-Dec-2025_17-04-08", [-3 50 35])
 
-strain_plotter("Max Bending 120 Deg","Max_Bend_120deg_1_30-Nov-2025_19-27-42", [-12 43 57])
-
-strain_plotter("Max Bending 300 Deg","max_comp_300_1_01-Dec-2025_16-55-37", [-15 45 55])
-strain_plotter("Max Bending 240 Deg","max_bend_240_1_01-Dec-2025_17-33-58", [-15 45 55])
-
-strain_plotter("Max Compression","max_comp_4_30-Nov-2025_18-46-49", [-8 44 49])
 
 
 
